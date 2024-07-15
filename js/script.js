b5739c8988bef92c6292a3c60be04979765f4703
Tobias Huppertz <tobias.huppertz@iu-study.org>
2024-07-15 14:37:48 +0200

    Added Search Overlay

--- js/script.js
+++ js/script.js
@@ -3,3 +3,8 @@ function open() {
   document.querySelector(".nav_menu").classList.toggle("show");
   document.querySelector(".hamburger_menu").classList.toggle("move");
 }
+
+document.querySelector(".search_icon").addEventListener("click", search);
+function search() {
+  document.querySelector(".nav_search").classList.toggle("show");
+}
