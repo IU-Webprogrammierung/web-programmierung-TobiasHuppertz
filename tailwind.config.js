505bd6e0f59ae9698739c6abc97b66535dd6b39d
Tobias Huppertz <tobias.huppertz@iu-study.org>
2024-07-28 13:24:27 +0200

    Changed text class form css to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -30,6 +30,14 @@ module.exports = {
         worksans: ["WorkSans", "sans-serif"],
         lobster: ["Lobster", "sans-serif"],
       },
+      fontWeight: {
+        smoothbold: '550',
+      },
+      textShadow: {
+        sm: '0 1px 2px var(--tw-shadow-color)',
+        DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+        lg: '0 8px 16px var(--tw-shadow-color)',
+      },
     },
   },
   plugins: [],
