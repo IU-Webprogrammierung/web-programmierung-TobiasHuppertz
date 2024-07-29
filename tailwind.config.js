fa54946a7b05f849cb56b7c402fc47526696a692
Tobias Huppertz <tobias.huppertz@iu-study.org>
2024-07-29 17:02:10 +0200

    Fixed Nav-Bar to top; Added Custom Class "up" for upper Shadow; Refactored index; Added Custom Class "backgroundImage" for responsive Background

--- tailwind.config.js
+++ tailwind.config.js
@@ -38,6 +38,13 @@ module.exports = {
         DEFAULT: '0 2px 4px var(--tw-shadow-color)',
         lg: '0 8px 16px var(--tw-shadow-color)',
       },
+      boxShadow: {
+        'up': '0 -4px 6px -1px rgba(0, 0, 0, 0.1), 0 -2px 4px -1px rgba(0, 0, 0, 0.06)',
+      },
+      backgroundImage: theme => ({
+        'custom-desktop': "url('/img/bg_desktop.jpg')",
+        'custom-mobile': "url('/img/bg_mobile.jpg')",
+      }),
     },
   },
   plugins: [],
